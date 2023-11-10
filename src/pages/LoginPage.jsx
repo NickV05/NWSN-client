99701ee966ffddad6d9d1c65a5e69aa8c7a5633e
Nikita Valovyy <nickwolf046@gmail.com>
2023-11-10 13:50:02 -0500

    login & signup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Link, useNavigate } from "react-router-dom";
+import { post } from "../services/authService";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <p>Login Page</p>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
 
-export default LoginPage
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+    post("/auth/login", requestBody)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <section>
+      <div>
+        <div class="md:w-4/12 lg:ml-6 lg:w-4/12">
+          <form onSubmit={handleLoginSubmit}>
+            <div>
+              <input
+                type="email"
+                name="email"
+                onChange={handleEmail}
+                placeholder="Email address"
+              />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                name="password"
+                onChange={handlePassword}
+                placeholder="Password"
+              />
+            </div>
+
+            <button type="submit">Sign in</button>
+
+            <div>
+              <p>
+                {errorMessage && <p>{errorMessage}</p>}
+                Don't have an account yet?
+                <Link to="/signup"> Sign Up</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import { post } from "../services/authService";
 
 const SignupPage = () => {
-  return (
-    <div>
-      <p>Signup Page</p>
-    </div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    fullName: "",
+    location: "",
+    username: "",
+  });
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const { storeToken, authenticateUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleTextChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
 
-export default SignupPage
+    post("/auth/signup", user)
+      .then((response) => {
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <section>
+      <div>
+        <div>
+          <div>
+            <form onSubmit={handleSignupSubmit}>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleTextChange}
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div>
+                <input
+                  type="password"
+                  name="password"
+                  id="exampleFormControlInput33"
+                  placeholder="Password"
+                  onChange={handleTextChange}
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  name="fullName"
+                  id="exampleFormControlInput33"
+                  placeholder="Full Name"
+                  onChange={handleTextChange}
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  name="username"
+                  id="exampleFormControlInput33"
+                  placeholder="Username"
+                  onChange={handleTextChange}
+                />
+              </div>
+
+              <div>
+                <input
+                  type="text"
+                  name="location"
+                  id="exampleFormControlInput33"
+                  placeholder="Location"
+                  onChange={handleTextChange}
+                />
+              </div>
+
+              <button type="submit">Sign up</button>
+
+              <div>
+                <p>
+                  {errorMessage && <p>{errorMessage}</p>}
+                  Have an account?
+                  <Link to="/login"> Sign in</Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignupPage;
