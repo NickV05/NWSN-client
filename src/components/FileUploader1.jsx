a6444d2a1628816ed13eefa878690b2ed1d70e98
Nikita Valovyy <nickwolf046@gmail.com>
2023-11-29 14:29:55 -0500

    Captcha issue

---
+++ src/components/FileUploader1.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+const FileUpload1 = () => {
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    const newFiles = [...e.target.files];
+    setFiles(newFiles);
+  };
+
+  const handleClear = () => {
+    setFiles([]);
+  };
+
+  return (
+    <div>
+      <div>
+        {files.map((file, index) => (
+          <div key={index}>{file.name}</div>
+        ))}
+      </div>
+      <input type="file" multiple onChange={handleFileChange} />
+      <button onClick={handleClear}>Clear</button>
+    </div>
+  );
+};
+
+export default FileUpload1;
--- src/pages/MemberForm.jsx
+++ src/pages/MemberForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { post } from '../services/authService';
 import { useNavigate } from 'react-router-dom';
+import FileUpload1 from '../components/FileUploader1';
+import FileUpload2 from '../components/FileUploader2';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const MemberForm = () => {
   const navigate = useNavigate();
@@ -75,6 +78,36 @@ const MemberForm = () => {
     { range: '$10,000,001 and Up', dues: 10000, persons: 4 },
   ];
 
+  const [name, setName] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const [captchaValue, setCaptchaValue] = useState(null);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name.length) {
+      if (!captchaValue) {
+        alert('Please complete the reCAPTCHA before submitting.');
+        return;
+      }
+      console.log('Name:', name);
+      console.log('Agreed to terms');
+      console.log('Form submitted:', { organizationInfo, mainContactInfo, membershipEligibility });
+      navigate("/confirmation");
+    } else {
+      alert('Please agree to the terms before submitting.');
+    }
+  };
+
+  const handleCaptchaChange = (value) => {
+    setCaptchaValue(value);
+  };
+
 
   const handleInputChange = (category, fieldName, value) => {
     switch (category) {
@@ -107,12 +140,6 @@ const MemberForm = () => {
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted:', { organizationInfo, mainContactInfo, membershipEligibility });
-    navigate("/confirmation");
-
-  };
 
   return (
     <section className="section">
@@ -598,11 +625,7 @@ const MemberForm = () => {
             type="checkbox"
             checked={additionalInfo.fees}
             onChange={(e) =>
-              handleInputChange(
-                "additionalInfo",
-                "fees",
-                e.target.checked
-              )
+              handleInputChange("additionalInfo", "fees", e.target.checked)
             }
           />
           Does your organization charge participants any fees, charges or dues?
@@ -620,7 +643,8 @@ const MemberForm = () => {
                 )
               }
             />
-            What types of fees, charges or dues, how much and are they sliding scale? 
+            What types of fees, charges or dues, how much and are they sliding
+            scale?
           </label>
         )}
 
@@ -629,143 +653,117 @@ const MemberForm = () => {
             type="checkbox"
             checked={additionalInfo.pay}
             onChange={(e) =>
-              handleInputChange(
-                "additionalInfo",
-                "pay",
-                e.target.checked
-              )
+              handleInputChange("additionalInfo", "pay", e.target.checked)
             }
           />
           Do you provide services regardless of a person’s ability to pay?
         </label>
 
         <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "howManyLocations",
-                  e.target.value
-                )
-              }
-            />
-            How many locations do you operate? 
-          </label>
-          
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  " howManyShelters",
-                  e.target.value
-                )
-              }
-            />
-            How many shelter beds do you have?
-          </label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange(
+                "additionalInfo",
+                "howManyLocations",
+                e.target.value
+              )
+            }
+          />
+          How many locations do you operate?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "howManyBedNights",
-                  e.target.value
-                )
-              }
-            />
-            Approximately how many shelter-bed nights do you provide annually?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange(
+                "additionalInfo",
+                " howManyShelters",
+                e.target.value
+              )
+            }
+          />
+          How many shelter beds do you have?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "units",
-                  e.target.value
-                )
-              }
-            />
-            If you provide transitional housing, how many units do you have and who do you serve?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange(
+                "additionalInfo",
+                "howManyBedNights",
+                e.target.value
+              )
+            }
+          />
+          Approximately how many shelter-bed nights do you provide annually?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "meals",
-                  e.target.value
-                )
-              }
-            />
-            Approximately how many free meals do you provide annually?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "units", e.target.value)
+            }
+          />
+          If you provide transitional housing, how many units do you have and
+          who do you serve?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "counseling",
-                  e.target.value
-                )
-              }
-            />
-            Approximately how many free counseling sessions do you provide annually?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "meals", e.target.value)
+            }
+          />
+          Approximately how many free meals do you provide annually?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "types",
-                  e.target.value
-                )
-              }
-            />
-            What other types of services do you provide?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "counseling", e.target.value)
+            }
+          />
+          Approximately how many free counseling sessions do you provide
+          annually?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "women",
-                  e.target.value
-                )
-              }
-            />
-            Approximately how many women do you serve annually?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "types", e.target.value)
+            }
+          />
+          What other types of services do you provide?
+        </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "children",
-                  e.target.value
-                )
-              }
-            />
-            Approximately how many children do you serve annually?
-          </label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "women", e.target.value)
+            }
+          />
+          Approximately how many women do you serve annually?
+        </label>
 
-          <label>
+        <label>
+          <input
+            type="text"
+            onChange={(e) =>
+              handleInputChange("additionalInfo", "children", e.target.value)
+            }
+          />
+          Approximately how many children do you serve annually?
+        </label>
+
+        <label>
           <input
             type="checkbox"
             checked={additionalInfo.association}
@@ -777,91 +775,180 @@ const MemberForm = () => {
               )
             }
           />
-          Is your organization an association, network, coalition or similar organization?
+          Is your organization an association, network, coalition or similar
+          organization?
         </label>
 
         {additionalInfo.association && (
-            <>
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "mission",
-                  e.target.value
-                )
-              }
-            />
-           Tell us what your mission is?
-          </label>
+          <>
+            <label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  handleInputChange("additionalInfo", "mission", e.target.value)
+                }
+              />
+              Tell us what your mission is?
+            </label>
 
-          <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "becomeMembers",
-                  e.target.value
-                )
-              }
-            />
-           Who can become members?
-          </label>
+            <label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  handleInputChange(
+                    "additionalInfo",
+                    "becomeMembers",
+                    e.target.value
+                  )
+                }
+              />
+              Who can become members?
+            </label>
 
-          <label>
-          <input
-            type="checkbox"
-            checked={additionalInfo.sheltering}
-            onChange={(e) =>
-              handleInputChange(
-                "additionalInfo",
-                "sheltering",
-                e.target.checked
-              )
-            }
-          />
-          Are your members sheltering, housing and/or providing services to persons experiencing or at risk of homelessness?
-        </label>
+            <label>
+              <input
+                type="checkbox"
+                checked={additionalInfo.sheltering}
+                onChange={(e) =>
+                  handleInputChange(
+                    "additionalInfo",
+                    "sheltering",
+                    e.target.checked
+                  )
+                }
+              />
+              Are your members sheltering, housing and/or providing services to
+              persons experiencing or at risk of homelessness?
+            </label>
 
-        <label>
-            <input
-              type="text"
-              onChange={(e) =>
-                handleInputChange(
-                  "additionalInfo",
-                  "howManyMembers",
-                  e.target.value
-                )
-              }
-            />
-           How many members do you have?
-          </label>
+            <label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  handleInputChange(
+                    "additionalInfo",
+                    "howManyMembers",
+                    e.target.value
+                  )
+                }
+              />
+              How many members do you have?
+            </label>
           </>
         )}
 
         <h4>NWSN Membership Dues are Based on a Sliding Scale:</h4>
-        <p>Please contact us if your organization would like to join the Network but cannot afford to pay dues</p>
+        <p>
+          Please contact us if your organization would like to join the Network
+          but cannot afford to pay dues
+        </p>
 
         <table>
-      <thead>
-        <tr>
-          <th>Annual Budget</th>
-          <th>Annual Dues $$ </th>
-          <th>Conference Registration Fee Waived for # of Persons ~ Big Saving!</th>
-        </tr>
-      </thead>
-      <tbody>
-        {duesData.map((item, index) => (
-          <tr key={index}>
-            <td>{item.range}</td>
-            <td>{item.dues}</td>
-            <td>{item.persons}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>     
+          <thead>
+            <tr>
+              <th>Annual Budget</th>
+              <th>Annual Dues $$ </th>
+              <th>
+                Conference Registration Fee Waived for # of Persons ~ Big
+                Saving!
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {duesData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.range}</td>
+                <td>{item.dues}</td>
+                <td>{item.persons}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <p>
+          What is Your Organization’s Most Recent Fiscal Year Annual Budget?
+        </p>
+        <p>
+          Please upload copies of your organization's IRS Form 990 and IRS 501c3
+          Determination Letter
+        </p>
+        <FileUpload1 />
+        <FileUpload2 />
+
+        <h4>Network Membership Terms</h4>
+        <p>
+          By e-signature below, our organization elects to join the NWSN’s
+          Network as a member and meets the membership eligibility criteria for
+          the Network. We understand that our organization will not become a
+          member of the Network unless our application is processed and accepted
+          by the NWSN. We will be notified by the NWSN of acceptance, at which
+          point our organization will promptly pay the required sliding scale
+          annual dues charged to participate in the Network and be deemed to
+          have joined the Network as a member in accord with these terms. Our
+          organization may withdraw its participation as a member of the Network
+          at any time by giving written notice to the NWSN, however, it will not
+          receive a refund of any previously paid dues or fees and the last
+          paragraph of these Network Membership Terms shall survive. We
+          understand that becoming a participating member of the Network does
+          not entitle our organization to any ownership right, title or interest
+          in the NWSN, nor can we speak on behalf of or legally bind in any way
+          the NWSN or Network. Membership in the Network is in the nature of a
+          subscription to participate in the Network’s benefits and activities
+          and to be counted in shared mission and vision. We understand that the
+          NWSN will negotiate and bind on behalf of the Network, free and
+          discounted goods and services with partners, preferred providers,
+          vendors and consultants, and others, and that these partners,
+          providers, vendors, consultants and others may contact our
+          organization to discuss the benefits they offer to the Network;
+          however, our organization’s decision to receive any goods and services
+          and participate in any of the offerings and benefits of the Network is
+          without obligation and entirely in our sole and absolute discretion.
+          We understand that a requirement of Network membership is to provide
+          annual data reports and/or surveys as requested; this information will
+          be de-identified (unless our organization’s permission is obtained)
+          and used by the NWSN to secure funding, negotiate free and discounted
+          goods and services, better serve the Network, and in furtherance of
+          its research and public policy initiatives, as well as for advocacy
+          and storytelling. Our organization agrees to be included in the NWSN
+          Network Directory and to complete the information needed for women to
+          find our shelter and safe haven programs. If we elect to withdraw from
+          the Network or fail to pay in full the annual dues charged by the
+          NWSN, our organization shall immediately cease using the NWSN Member
+          logo, will no longer have access to the private sections of the NWSN
+          web-based platform reserved for Members, and be removed from the NWSN
+          Network Directory. Our organization understands that the NWSN and its
+          funders, partners, providers, vendors, consultants, volunteers and
+          others provide to the Network a wide range of information, education,
+          training, resources, goods, services, funding and other benefits,
+          however, it is each organization’s responsibility to determine whether
+          any of the foregoing are acceptable and suitable for their use,
+          activities (including any further distribution thereof) and purposes,
+          seek legal counsel, and comply with all federal, state and local laws,
+          rules, regulations and other requirements applicable thereto. The NWSN
+          makes no warranties or guarantees of any kind in regard to such
+          information, education, training, resources, goods, services, funding
+          and other benefits, including without limitation suitability for any
+          person’s particular use, activities (including any further
+          distribution thereof) and purposes, and shall have no liability in
+          connection with or arising out of any of the foregoing, all of which
+          shall be the sole responsibility of such organization. This paragraph
+          shall survive any termination of membership in the Network.
+        </p>
+        <p>
+          By typing my name below, our organization agrees to the above terms of
+          membership in the NWSN Network.
+        </p>
+
+        <label>
+        Signature:
+          <input type="text" value={name} onChange={handleNameChange} />
+        </label>
+        <br />
+
+        <ReCAPTCHA
+          sitekey="6LcN6SApAAAAAJQ50g6aK44wnSs1U_Ccc5VeXPAp" 
+          onChange={handleCaptchaChange}
+        />
 
         <button type="submit">Submit</button>
       </form>
